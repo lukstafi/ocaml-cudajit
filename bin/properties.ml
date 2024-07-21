8dff28f688e290f725ad10db954daf507ec01931
Lukasz Stafiniak <lukstafi@gmail.com>
2024-07-21 22:27:52 +0200

    bin/properties: Print default limits

--- bin/properties.ml
+++ bin/properties.ml
@@ -12,5 +12,18 @@ let () =
       let ctx_flags = Cu.ctx_get_flags () in
       Format.printf "GPU #%d:@ %a@\nContext properties:@ %a@\n%!" ordinal Sexplib0.Sexp.pp_hum
         (Cu.sexp_of_device_attributes props)
-        Sexplib0.Sexp.pp_hum (Cu.sexp_of_ctx_flags ctx_flags))
+        Sexplib0.Sexp.pp_hum (Cu.sexp_of_ctx_flags ctx_flags);
+      Format.printf "Default limits:@ %a\n%%!" Sexplib0.Sexp.pp_hum
+        (Sexplib0.Sexp_conv.(
+           sexp_of_list (fun li ->
+               sexp_of_pair Cu.sexp_of_limit sexp_of_int (li, Cu.ctx_get_limit li)))
+           [
+             STACK_SIZE;
+             PRINTF_FIFO_SIZE;
+             MALLOC_HEAP_SIZE;
+             DEV_RUNTIME_SYNC_DEPTH;
+             DEV_RUNTIME_PENDING_LAUNCH_COUNT;
+             MAX_L2_FETCH_GRANULARITY;
+             PERSISTING_L2_CACHE_SIZE;
+           ]))
     gpus
